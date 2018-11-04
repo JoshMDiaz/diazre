14b7f141a95ad5aae5b8659f070abe85d5ffc4f8
Josh Diaz <josh@iserveproducts.com>
2018-11-03 19:19:07 -0600

    update styles

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -13,14 +13,14 @@ const goToPricing = () => {
 
 const About = () => {
   return (
-    <div className="padding max-width">
+    <div className="about padding max-width">
       <Slide left>
         <h2>About Us</h2>
       </Slide>
       <p>
         Diaz Real Estate, LLC specializes in Virtual Reality Photography in real estate. If you are listing a home, you will definitely want to have a virtual tour of your home. Buyers can get a much better feel for the space by touring your home online than by viewing a few pictures. Don't miss this opportunity!
       </p>
-      <button onClick={goToPricing}>Pricing</button>
+      <button onClick={goToPricing}>See Pricing</button>
     </div>
   );
 }
