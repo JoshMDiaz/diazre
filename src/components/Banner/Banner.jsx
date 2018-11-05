508ecf515c9c52516724292760ba7deccec9f459
Josh Diaz <josh@iserveproducts.com>
2018-11-04 17:25:47 -0700

    mobile youtube fix

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -6,7 +6,7 @@ import Pulse from 'react-reveal/Pulse'
 const Banner = () => {
   return (
     <div className="banner">
-      <iframe title="bg-video" width="1920" height="1080" src="https://www.youtube.com/embed/MhkRUQuYza4?autoplay=1&loop=1&playlist=MhkRUQuYza4&controls=0&fs=0&rel=0&showinfo=0&disablekb=1" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen className="banner-media"></iframe>
+      <iframe title="bg-video" width="1920" height="1080" src="https://www.youtube.com/embed/MhkRUQuYza4?autoplay=1&loop=1&playlist=MhkRUQuYza4&controls=0&fs=0&rel=0&showinfo=0&disablekb=1&mute=1" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen className="banner-media"></iframe>
       <div className="lines">
         { Array.apply(1, {length: 6}).map((e, i) => (
           <Slide down delay={Number(500 * (i + 1))} key={i}>
