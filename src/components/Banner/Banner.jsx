dd213e6e7e34e6214608e0eb8fd870ad50072b85
Josh Diaz <josh@iserveproducts.com>
2018-12-18 20:44:13 -0700

    banner and header update

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
-import downArrow from '../../images/down_arrow.svg'
-import Slide from 'react-reveal/Slide'
-import Pulse from 'react-reveal/Pulse'
 import bannerImg from '../../images/banner1.jpg'
+import Title from '../Title/Title'
 
 const Banner = () => {
   return (
@@ -11,23 +9,10 @@ const Banner = () => {
       backgroundPosition: 'center',
       backgroundSize: 'cover'
     }}>
-      <div className="lines">
-        { Array.apply(1, {length: 6}).map((e, i) => (
-          <Slide down delay={Number(150 * (i + 1))} key={i}>
-            <div className="line"></div>
-          </Slide>
-        ))}
-      </div>
       <div className="banner-content">
         <h1>Virtual Tour Photography</h1>
-        <h4>Showing off your unique space</h4>
+        <Title beforeText="Showing off your">unique space</Title>
       </div>
-      <Pulse delay={5000} forever>
-        <div className="banner-footer padding max-width">
-          <h3>Scroll</h3>
-          <img src={downArrow} alt="down arrow"/>
-        </div>
-      </Pulse>
     </div>
   );
 }
