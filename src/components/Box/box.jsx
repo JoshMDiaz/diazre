2c4c39d6fc724945120073655f7592f46e0849d1
Josh Diaz <josh@iserveproducts.com>
2018-12-22 21:50:24 -0700

    working on box styling

--- src/components/Box/box.jsx
+++ src/components/Box/box.jsx
@@ -7,13 +7,6 @@ import {
   DialogContent
 } from "@reach/dialog"
 
-const fillerColors = [
-  '#b8d4e2',
-  '#51a8d0',
-  '#5b7784',
-  '#deeef6'
-]
-
 class Box extends Component {
   constructor(props) {
     super(props);
@@ -26,41 +19,19 @@ class Box extends Component {
     this.setState({ modalOpen: opened });
   }
 
-  randomColor = () => {
-    for (let i = fillerColors.length - 1; i >= 0; i--) {
-      let randomIndex = Math.floor(Math.random()*(i+1)),
-          itemAtIndex = fillerColors[randomIndex]; 
-      fillerColors[randomIndex] = fillerColors[i]; 
-      fillerColors[i] = itemAtIndex;
-    }
-    return fillerColors;
-  }
-
   render() { 
     const { image, name, num } = this.props,
           { modalOpen } = this.state;
-    let fillerBgs = this.randomColor();
     return (
       <div>
         <div className={`box ${num % 2 ? 'even' : 'odd'}`} onClick={() => this.toggleModal(true)}>
           <Slide up>
-            <div className={`home-picture`} style={{
-              backgroundImage: `linear-gradient(0deg, rgba(4, 21, 51, 0.4), rgba(4, 21, 51, 0.5)), url(${image})`,
-              backgroundPosition: 'center',
-              backgroundSize: 'cover'
-            }}>
-              <div className={`home-info`}>
-                <h3>{name}</h3>
-              </div>
+            <div className={`home-picture`}>
+              <img src={image} alt={name}/>
+              <div className="overlay"></div>
+              <h3>View</h3>
             </div>
           </Slide>
-          <div className="fillers">
-            { Array.apply(1, {length: 4}).map((f, j) => (
-              <Slide left={j === 2 || j === 0} right={j === 1 || j === 3}>
-                <div className={`filler`} key={j} style={{ background: fillerBgs[j] }}></div>
-              </Slide>
-            ))}
-          </div>
         </div>
         <DialogOverlay isOpen={modalOpen}>
           <DialogContent>
