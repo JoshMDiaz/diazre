64b172bf9082441d582e9112e7e4168562b263e9
Josh Diaz <josh@iserveproducts.com>
2018-12-22 21:13:00 -0700

    Removing section component, button styling

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,26 +2,23 @@ import React from 'react'
 import Slide from 'react-reveal/Slide'
 import InterestedButton from '../Interested/Interested'
 import Title from '../Title/Title'
-import Section from '../Section/Section';
 
 const About = () => {
   return (
     <div className="about padding max-width" name="who-we-are">
-      <Section>
-        <Slide down>
-          <Title beforeText="Why you should">
-            <span>work with us</span>
-          </Title>
-        </Slide>
-        <p>Diaz Real Estate, LLC specializes in Virtual Tour Photography for real estate. If you are listing a home or if you simply want to show off your unique space, you need to have a virtual tour!</p>
-        <label>What is a Virtual Tour?</label>
-        <p>It is an immersive 360<sup>o</sup> walkthrough of a location. Your customers will feel like they are walking through your beautiful home, one-of-a-kind garden, top-notch hotel, unique bed and breakfast, or whatever the space that you choose to create a tour of.</p>
-        <label>I already have pictures...</label>
-        <p>Great! High quality photos, along with a virtual tour, will surely get your customers excited about visiting! They will get a much better feel for the space by taking a virtual tour online before they decide to make the trip out and see it in person.</p>
-        <label>Who should use us?</label>
-        <p>Simply put - anyone with a business and a physical space! For real estate agents, this may be the push that your potential buyers need to stop by and see your property. Wish more people would come to your bed and breakfast? Do you host weddings in your amazing backyard or unique building? The applications are endless! Don't miss this opportunity!</p>
-        <InterestedButton text="Get a Quote" />
-      </Section>
+      <Slide down>
+        <Title beforeText="Why you should">
+          <span>work with us</span>
+        </Title>
+      </Slide>
+      <p>Diaz Real Estate, LLC specializes in Virtual Tour Photography for real estate. If you are listing a home or if you simply want to show off your unique space, you need to have a virtual tour!</p>
+      <label>What is a Virtual Tour?</label>
+      <p>It is an immersive 360<sup>o</sup> walkthrough of a location. Your customers will feel like they are walking through your beautiful home, one-of-a-kind garden, top-notch hotel, unique bed and breakfast, or whatever the space that you choose to create a tour.</p>
+      <label>I already have pictures...</label>
+      <p>Great! High quality photos, along with a virtual tour, will surely get your customers excited about visiting! They will get a much better feel for the space by taking a virtual tour online before they decide to make the trip out and see it in person.</p>
+      <label>Who should use us?</label>
+      <p>Simply put - anyone with a business and a physical space! For real estate agents, this may be the push that your potential buyers need to stop by and see your property. Wish more people would come to your bed and breakfast? Do you host weddings in your amazing backyard or unique building? The applications are endless! Don't miss this opportunity!</p>
+      <InterestedButton text="Get a Quote" />
     </div>
   );
 }
--- src/components/Pricing/Pricing.jsx
+++ src/components/Pricing/Pricing.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
-// import carImg from '../../images/car.svg'
-// import houseImg from '../../images/house.svg'
 import Slide from 'react-reveal/Slide'
 import InterestedButton from '../Interested/Interested'
-import Section from '../Section/Section'
+// import Section from '../Section/Section'
 import Title from '../Title/Title'
+import Particles from 'react-particles-js'
 
 const AboutPricing = () => {
   return (
-    <div className="pricing padding max-width" name="get-a-quote">
-      <Section>
-        <Slide down>
-          <Title beforeText="Just ask us">for a quote</Title>
-        </Slide>
-        <p className="description">The pricing for interior VR Tours are based on the square footage of the house. Exterior VR Tours available upon request - price will vary based on the acreage of the area needing to be covered. Please contact us to get a quote!</p>
-        <InterestedButton text="Get a Quote" />
-      </Section>
+    <div className="pricing" name="get-a-quote">
+      <div className="max-width padding">
+        <Particles style={{ opacity: '.3'}}
+        />
+        <div className="pricing-content">
+          <Slide down>
+            <Title beforeText="Just ask us">for a quote</Title>
+          </Slide>
+          <p className="description">The pricing for interior VR Tours are based on the square footage of the house. Exterior VR Tours available upon request - price will vary based on the acreage of the area needing to be covered. Please contact us to get a quote!</p>
+          <InterestedButton text="Get a Quote" />
+        </div>
+      </div>
     </div>
   );
 }
